825449e9e12bb1daf03c81b17a51cd9b80f45112
Espedito Melo <espedito.melo@gmail.com>
2024-04-23 09:46:53 -0300

    implement id do aluno em foto e persistência em bd

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,18 +1,26 @@
 import multer from 'multer';
 import multerConfig from '../config/multerConfig';
 
+import Foto from '../models/Foto';
+
 const upload = multer(multerConfig).single('foto');
 
 
 class FotoController {
+  // talvez tenha que remover esse async
   async store(req, res) {
-    return upload(req, res, (error) => {
+    return upload(req, res, async (error) => {
       if(error) {
         return res.status(400).json({
           erros: [error.code]
         });
       }
-      return res.json(req.file);
+
+      const { originalname, filename } = req.file;
+      const { aluno_id } = req.body;
+      const foto = await Foto.create({ originalname, filename, aluno_id });
+
+      return res.json(foto);
     });
   }
 }
